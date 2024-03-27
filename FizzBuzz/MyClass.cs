b7f81ebe9c9d1b9f7e89eda5610f4991ea2be996
ytr <danistus3@gmail.com>
2024-03-27 23:47:05 +0700

    feat: init

---
+++ FizzBuzz/MyClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class MyClass
+    {
+        Dictionary<int, string> rules;
+        public MyClass()
+        {
+            rules = new Dictionary<int, string>();
+            
+        }
+        public void addRule(int input, string output)
+        {
+            this.rules.Add(input, output);
+        }
+        
+        public void print(int maxNumber)
+        {
+            
+            for (int i = 1; i <= maxNumber; i++)
+            {
+                string display = "";
+                foreach (KeyValuePair<int, string> rule in rules)
+                {
+                   if(i % rule.Key == 0)
+                        display+= rule.Value;
+                }
+
+                if (display == "")
+                    Console.Write("" + i +", ");
+                else
+                    Console.Write(display + ", ");
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Case number 1");
+            MyClass myClass = new MyClass();
+            myClass.addRule(3, "foo");
+            myClass.addRule(5, "bar");
+            myClass.print(15);
+
+            Console.WriteLine("\nCase number 2");
+            myClass.addRule(7, "jazz");
+            myClass.print(35);
+
+            Console.WriteLine("\nCase number 3");
+            myClass.addRule(4, "baz");
+            myClass.addRule(9, "huzz");
+            myClass.print(35);
+
+            Console.ReadLine();
+        }
+    }
+}
